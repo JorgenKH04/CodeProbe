5ca5c6fe5037714a754ef2142ed766076e988dfd
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-11-07 10:28:01 +0100

    feat(Components): :sparkles: Searchbar component
    Added component for searching github users

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,10 @@
+export function Searchbar() {
+  return (
+    <div>
+      <input placeholder="Who you looking for cutie?" />
+      <button type="button">
+        <svg />
+      </button>
+    </div>
+  );
+}
