63fb96942cc987734569e5166d1f0c68551c5ef8
rond <contact@rond.cc>
2023-11-23 11:21:24 +0100

    feat(card): Implement modular card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Card from "@/components/Card";
-import Image from "next/image";
+import { Card, CardImg, CardInfo, CardBadge, CardBadges, CardName, CardShare } from '@/components/Card';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Nuxt</h1>
-      <Card />
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <Card>
+        <CardImg>
+          <img src='https://avatars.githubusercontent.com/u/30192333?v=4' alt='user' className='h-[6em] rounded-md' />
+        </CardImg>
+        <CardInfo>
+          <CardName>rondDev</CardName>
+          <CardBadges>
+            <CardBadge>11 followers</CardBadge>
+            <CardBadge>11 following</CardBadge>
+            <CardBadge>8 repos</CardBadge>
+          </CardBadges>
+        </CardInfo>
+        <CardShare>
+          <a href={'https://google.com'}>share</a>
+        </CardShare>
+      </Card>
     </main>
   );
 }
