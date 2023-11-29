9c9aefd02b2b37a9c705da13f8c116eb49c6c893
JorgenKH04 <131149458+JorgenKH04@users.noreply.github.com>
2023-11-29 12:22:38 +0100

    fix(Components): :bug: Fixed Searchbar component
    Removed useless userstore, fixed fetch request, function now returns data using api url

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,18 +1,32 @@
-import { useUserStore } from "@lib/state/currentUser";
+"use client";
+import React from "react";
+
+let search = "";
+const handleClick = (e: React.MouseEvent) => {
+  e.preventDefault();
+  GetUser(search);
+};
 
 async function GetUser(search: string) {
-  const res = await fetch(`/api/search/users?q=${search}`);
+  const res = await fetch(`/api/search/${search}`);
   const data = await res.json();
-  const user: string = useUserStore(() => user);
-  const setNewUser = useUserStore((state) => state.setUser(search));
 }
 
 export function Searchbar() {
   return (
     <div>
-      <input placeholder="Who you looking for cutie?" />
-      <button type="button">
-        <svg />
+      <input
+        id="UserSearch"
+        placeholder="Search for user"
+        onChange={(e) => {
+          search = e.target.value;
+        }}
+      />
+      <button type="button" onClick={handleClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>magnify by Material Design icons</title>
+          <path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+        </svg>
       </button>
     </div>
   );
